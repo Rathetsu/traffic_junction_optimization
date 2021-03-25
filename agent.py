765510289ba863af8d4b920351a4c724ca514acc
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2021-03-25 20:59:21 +0200

    optimization and bug fixes

--- agent.py
+++ agent.py
@@ -2,7 +2,7 @@ import random
 
 YELLOW_TIME = 6
 TOTAL_HOLD = 12
-sigma = 3
+sigma = 20
 alpha = 2
 taw = 7
 
@@ -28,10 +28,11 @@ class Agent:
 
         self.time_counter += 1
 
-        if self.time_counter == taw:
+        if self.time_counter >= taw:
             self.action = self.act(self.curr_state)
-            self.time_counter = 0
-        
+            if self.prev_action != self.action:
+                self.time_counter = 0
+            
         self.prev_state = self.curr_state
         self.prev_action_was_flip = True if self.prev_action != None and self.prev_action != self.action else False
         self.prev_action = self.action
@@ -48,6 +49,10 @@ class Agent:
         W = state[4] + (sigma * state[8])
 
         lane_weights = [N, E, S, W]
+
+        if not any(lane_weights):
+            return self.action
+
         action = lane_weights.index(max(lane_weights))
         #print('************************************ action:', action)
 
@@ -57,15 +62,15 @@ class Agent:
 
         for i in range(1, 9):
             if state[i] == -1:
-                if (i in [1, 5] and state[0] == 2) \
-                or (i in [2, 6] and state[0] == 0) \
-                or (i in [3, 7] and state[0] == 3) \
-                or (i in [4, 8] and state[0] == 1):
+                if (i in [1, 5] and state[0] == 0) \
+                or (i in [2, 6] and state[0] == 2) \
+                or (i in [3, 7] and state[0] == 1) \
+                or (i in [4, 8] and state[0] == 3):
                     state[i] = 0
 
         for i in range(1, 9):
             if state[i] == -1 and self.prev_state != None:
-                state[i] = self.prev_state[i] + 6 + 6 * self.prev_action_was_flip if self.prev_state[i] != 0 else -1
+                state[i] = self.prev_state[i] + 6 + 6 * int(self.prev_action_was_flip) if self.prev_state[i] != 0 else -1
             if state[i] == -1:
                 state[i] = 0 if i >= 5 or state[i + 4] == 0 else state[i + 4] + alpha
 
